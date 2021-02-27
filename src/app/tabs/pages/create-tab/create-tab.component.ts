a85983ac768a55e4174aaebfb3d9e92c8c357c04
michael peterson <michaeljoepeterson@gmail.com>
2021-02-26 19:51:19 -0700

    checkout

--- src/app/tabs/pages/create-tab/create-tab.component.ts
+++ src/app/tabs/pages/create-tab/create-tab.component.ts
@@ -42,7 +42,6 @@ export class CreateTabComponent implements OnInit {
     });
     this.subscriptions.push(routerSub);
     */
-
     const tab = this.route.snapshot.paramMap.get('tab');
     //console.log(tab);
     
